fd46ae3a63f7ba1e73079434827a140cf426bb7c
ankh <1832697406@qq.com>
2023-05-02 12:33:59 +0800

    优化了首页展示预约列表，去掉了过期的预约

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -97,7 +97,11 @@
     const getBookList = async () => {
         const res = await axios.get('/admin/equipment/getBookList')
         // console.log(res.data.data)
-        tableList.splice(0, tableList.length, ...res.data.data)
+        // 筛选出未过期的
+        const notExpired = res.data.data.filter(item => {
+            return item.book_date >= dayjs().startOf('day').valueOf()
+        })
+        tableList.splice(0, tableList.length, ...notExpired)
     }
     onMounted(() => {
         getBookList()
